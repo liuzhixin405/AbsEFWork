a2966fb8c336ce0293d0df763156359080a2ba43
liuzhixin405 <164910441@qq.com>
2023-04-19 16:51:05 +0800

    u
    u

--- AbsEFWork/Implementations/EFCoreProductService.cs
+++ AbsEFWork/Implementations/EFCoreProductService.cs
@@ -14,42 +14,5 @@ namespace BaseEntityFramework.Implementations
         public EFCoreProductService(ProductDbContext context) : base(context)
         {
         }
-
-        public Task<bool> Add(Product entity)
-        {
-          return base.Add(entity);
-        }
-
-        public Task<bool> Delete(Product entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Product>> GetAll()
-        {
-            var result = base.GetAll();
-            return result;
-        }
-
-        public Task<Product> GetEntity(Expression<Func<Product, bool>> expression)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<IReadOnlyCollection<Product>> GetList(Expression<Func<Product, bool>> expression)
-        {
-            var result = await _dbContext.Products.Where(expression).ToListAsync();
-            return result.AsReadOnly();
-        }
-
-        public Task<PageResult<Product>> GetPageResult<Req>(PageInput<Req> pagInput) where Req : new()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> Update(Product entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
